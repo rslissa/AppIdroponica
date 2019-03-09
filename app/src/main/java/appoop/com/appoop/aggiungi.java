3f421dab9460e7dda2a9beb2774cfaacd3a104a7
rosario_lissandrello <rs.lissa1997@gmail.com>
2019-03-09 11:38:43 +0100

    first commit

---
+++ app/src/main/java/appoop/com/appoop/aggiungi.java
@@ -0,0 +1,13 @@
+package appoop.com.appoop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aggiungi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_aggiungi);
+    }
+}

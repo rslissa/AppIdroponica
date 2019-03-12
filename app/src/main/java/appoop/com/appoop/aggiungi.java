8e3ccc2a5d7c156e401189de8ef6e4872ff03a8f
pakyr <p.roseti18@gmail.com>
2019-03-12 12:10:36 +0100

    prova git1

--- app/src/main/java/appoop/com/appoop/aggiungi.java
+++ app/src/main/java/appoop/com/appoop/aggiungi.java
@@ -2,7 +2,7 @@ package appoop.com.appoop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//ciao sono pasquale
+//ciao sono pasquale e il commento lo modifica pasusle
 public class aggiungi extends AppCompatActivity {
 
     @Override

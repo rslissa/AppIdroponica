aed4e89e362af31e35b90c2c288d07d475dbc946
rosario_lissandrello <rs.lissa1997@gmail.com>
2019-03-15 16:06:12 +0100

    implementazione del calendario per selezionare la data

--- app/src/main/java/appoop/com/appoop/aggiungi.java
+++ app/src/main/java/appoop/com/appoop/aggiungi.java
@@ -1,18 +1,18 @@
 package appoop.com.appoop;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.lang.reflect.Array;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.List;
+
 import java.util.Locale;
 
 public class aggiungi extends AppCompatActivity implements View.OnClickListener {
@@ -20,7 +20,7 @@ public class aggiungi extends AppCompatActivity implements View.OnClickListener 
     Serra serra1;
     Serra serra2;
     Serra serra3;
-    Date data=null;
+    public static Date data=null;
     String s;
     public aggiungi() {
         serre = new ArrayList<Serra> ();
@@ -61,34 +61,42 @@ public class aggiungi extends AppCompatActivity implements View.OnClickListener 
 
     public void onClick(View v) {
 
-        EditText nameField = (EditText) findViewById(R.id.editText_nome);
-        serra1.setSerra ( nameField.getText().toString());
-        nameField = (EditText) findViewById(R.id.editText_m2);
-        serra1.setM2 ( nameField.getText().toString());
-        nameField = (EditText) findViewById(R.id.editText3_coltura);
-        serra1.setColtura ( nameField.getText().toString());
-        nameField = (EditText) findViewById(R.id.editText4_varieta);
-        serra1.setVarieta ( nameField.getText().toString());
-        try{
-            nameField = (EditText) findViewById(R.id.editText_trapianto);
-            DateFormat formatoData = DateFormat.getDateInstance(DateFormat.SHORT, Locale.ITALY);
-            data = formatoData.parse(nameField.getText().toString());
-            formatoData.setLenient(false);
-            serra1.setTrapianto (data);
-        } catch (ParseException e) {
-            System.out.println("Formato data non valido.");
-        }
-        nameField = (EditText) findViewById(R.id.editText_LOin);
-        serra1.setLOentrata (Double.valueOf (nameField.getText().toString()));
-        nameField = (EditText) findViewById(R.id.editText_LOout);
-        serra1.setLOsgrondo (Double.valueOf (nameField.getText().toString()));
-        nameField = (EditText) findViewById(R.id.editText_EC);
-        serra1.setTargetEC (Double.valueOf (nameField.getText().toString()));
+        EditText Tnome = findViewById(R.id.editText_nome);
+        EditText Tm2 = findViewById(R.id.editText_m2);
+        EditText Tcoltura = findViewById(R.id.editText3_coltura);
+        EditText Tvarieta = findViewById(R.id.editText4_varieta);
+        EditText TLOin = findViewById(R.id.editText_LOin);
+        EditText TLOout = findViewById(R.id.editText_LOout);
+        EditText TEC = findViewById(R.id.editText_EC);
+
+        serra1.setSerra (Tnome.getText().toString());
+        serra1.setM2 ( Tm2.getText().toString());
+        serra1.setColtura ( Tcoltura.getText().toString());
+        serra1.setVarieta ( Tvarieta.getText().toString());
+        serra1.setTrapianto (data);
+        serra1.setLOentrata (Double.valueOf (TLOin.getText().toString()));
+        serra1.setLOsgrondo (Double.valueOf (TLOout.getText().toString()));
+        serra1.setTargetEC (Double.valueOf (TEC.getText().toString()));
         serre.add (serra1);
 
         for(Iterator<Serra> i= serre.iterator (); i.hasNext ();){
             Serra s=i.next ();
             System.out.println(s);
+        }
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static void setData(String Sdata) {
+        try {
+
+            DateFormat formatoData = DateFormat.getDateInstance(DateFormat.SHORT, Locale.ITALY);
+            data = formatoData.parse(Sdata);
+        } catch (ParseException e) {
+            e.printStackTrace ( );
         }
     }
 
